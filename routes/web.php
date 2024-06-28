836a5c8b42d08bd2eda812301324e7d67b9d774e
Antonino <drakon9927@libero.it>
2024-06-28 23:57:52 +0200

    capitolo 1

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('Ciao');
+});
+
+Route::get('/Laravel', function () {
+    return view('Laravel');
 });
