11283555a2775005631750f7e66a1d126a0e05af
Antonino <drakon9927@libero.it>
2024-06-29 00:04:00 +0200

    capitolo 2

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('Ciao');
+    $data = ['dato1', 'dato2', 'dato3'];
+    return view('home', compact('data'));
 });
 
-Route::get('/Laravel', function () {
-    return view('Laravel');
+Route::get('/home', function () {
+    $data = ['dato1', 'dato2', 'dato3'];
+    return view('home', compact('data'));
 });
